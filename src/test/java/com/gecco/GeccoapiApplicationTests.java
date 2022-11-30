717a33be8b223561113145164ac07a7637db31d4
Julian75 <117301639+Julian75@users.noreply.github.com>
2022-11-30 14:15:45 -0500

    Add files via upload

---
+++ src/test/java/com/gecco/GeccoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gecco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeccoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
